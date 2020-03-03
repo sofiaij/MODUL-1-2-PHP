7b7fa1c84b6570db98dc7b908356921e964d7123
sofiaij <54301527+sofiaij@users.noreply.github.com>
2020-03-03 09:29:31 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Sofia";
+echo "<br>";
+echo "Aku sedang belajar PHP di SMK Tekom Malang";
+?>
---
+++ latihan3b.php
@@ -0,0 +1,18 @@
+<?php
+$a=3;
+$b=2;
+
+echo $a."<br/>";          //a = 3 akan tampil 3
+$a += 1;                  //a berisi 3 + 1
+echo $a."<br/>";        
+$a -= $b;                 //a berisi 4 - 2
+echo $a."<br/>";          
+$a *= $b;                 //a berisi 2 *  2
+echo $a."<br/>";         
+$a /= $b;                 //a beirsi 4 / 2
+echo $a."<br/>";         
+$a .= $b;                 //a berisi 2.2
+echo $a."<br/>";          
+$a %= $b;                 //a berisii 22 % 2
+echo $a."<br/>";
+?>
